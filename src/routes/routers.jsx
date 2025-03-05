32bb55c5d7019d237c3c21c4a3368150bd417f2e
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2025-03-05 16:29:55 +0600

    data displayed in UI

--- src/routes/routers.jsx
+++ src/routes/routers.jsx
@@ -1,31 +1,41 @@
-import React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
+import React from 'react';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 import HomeLayout from '../components/Layouts/HomeLayout';
+import News from '../components/Main/News';
 
-const router = createBrowserRouter(
-    [
-
+const router = createBrowserRouter([
         {
-                path : '/',
-                element : <HomeLayout/>
+                path: '/',
+                element: <HomeLayout />,
+                children: [
+                        {
+                                path: '/', // Default path
+                                element: <Navigate to="/categories/0" />, // Redirect to /categories/0
+                        },
+                        {
+                                path: '/categories/:id',  // Keep the same path
+                                element: <News />,
 
+                                // Log to verify
+
+
+
+
+                        },
+                ],
         },
         {
-                path : '/news',
-                element : <h1>nes updated</h1>
-
+                path: '/news',
+                element: <h1>News updated</h1>,
         },
         {
-                path : '/auth',
-                element : <h1>auth here</h1>
-
+                path: '/auth',
+                element: <h1>Authentication page</h1>,
         },
         {
-                path :'*',
-                element : <h1>error</h1>
-
+                path: '*',
+                element: <h1>Page not found</h1>,
         },
+]);
 
-    ]
-)
 export default router;
